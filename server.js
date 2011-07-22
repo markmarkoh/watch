a418d5afdd24ab10f5d2699d8e6355bba7af3d86
Charles Himmer <charleshimmer@gmail.com>
2011-07-22 07:07:04 -0500

    added comments
    Added comments to server.js to make it easier to follow what is going on.

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ var io = require("socket.io").listen(7201);
 var fs = require("fs");
 
 io.sockets.on('connection', function(socket) {
-        socket.emit('news', {hello : 'world'});
-        
-        var watching = [];
-        
-        socket.on('watchfile', function(data) {
-            if (data.name.length && !(data.name in watching) ) {
-              fs.watchFile(data.name, {interval: 50}, function(curr, prev) {
+
+    // initial connection
+    socket.emit('connection successful', {hello : 'world'});
+
+    // keep track of watched files
+    var watching = [];
+    
+    // when the client gives us a file to watch add watch listener    
+    socket.on('watchfile', function(data) {
+
+        // if we got a file and it isn't already being watched
+        if (data.name.length && !(data.name in watching) ) {
+
+            fs.watchFile(data.name, {interval: 100}, function(curr, prev) {
+                // send client notification of change if this file was modified and not just accessed
                 if (curr.mtime > prev.mtime) {
-                  socket.emit("file changed", {file: curr, location: data.location, type : data.type});
+                    socket.emit("file changed", {file: curr, location: data.location, type : data.type});
                 }
-              });
-              
-              watching.push(data.name);
-            }
-        });
+            });
+            
+            // add to array of watched files
+            watching.push(data.name);
+        }
+    });
 });
